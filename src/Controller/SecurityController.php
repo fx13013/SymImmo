54412cffe70aeb57eb86859979199e7c0ee368b6
Fx BTG <52882967+fx13013@users.noreply.github.com>
2020-06-01 16:14:46 +0200

    Mise en place de l'authentification

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        return $this->render('security/login.html.twig', [
+            'error' => $error
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout()
+    {
+        return $this->redirectToRoute('security_login');
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,15 +3,40 @@
 namespace App\DataFixtures;
 
 use App\Entity\Property;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
+
+        $admin = new User();
+        $admin->setFullName('John Doe')
+            ->setEmail('admin@gmail.com')
+            ->setPassword($this->encoder->encodePassword($admin, 'admin'))
+            ->setRoles(['ROLE_ADMIN']);
+        $manager->persist($admin);
+
+        for($u = 0; $u < 10; $u++){
+            $user = new User;
+            $user->setFullName($faker->name())
+                ->setEmail("user$u@gmail.com")
+                ->setPassword($this->encoder->encodePassword($user, 'password'));
+
+            $manager->persist($user);
+        }
 
         for($p = 0; $p < 100; $p++){
             $property = new Property;
