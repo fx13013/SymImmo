84cc4e3791efab0e354e0827bcd6182ffae3d0b7
Fx BTG <52882967+fx13013@users.noreply.github.com>
2020-06-02 23:38:15 +0200

    Gestion des options

--- src/Form/PropertyType.php
+++ src/Form/PropertyType.php
@@ -2,9 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Option;
 use App\Entity\Property;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -40,6 +43,11 @@ class PropertyType extends AbstractType
             ->add('heat', ChoiceType::class, [
                 'label' => 'Type de chauffage',
                 'choices' => $this->getChoices()
+            ])
+            ->add('options', EntityType::class, [
+                'class' => Option::class,
+                'choice_label' => 'name',
+                'multiple' => true
             ])
             ->add('city', TextType::class, [
                 'label' => 'Ville'
